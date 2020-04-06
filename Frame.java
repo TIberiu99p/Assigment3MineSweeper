3e6e18057d7eac109f52655b40e481605038aa89
Tiberius <35663392+TIberiu99p@users.noreply.github.com>
2020-04-07 00:47:16 +0100

    Update Frame.java
    Kill me

--- Frame.java
+++ Frame.java
@@ -1,8 +1,3 @@
-
-
-//TODO Do the button correctly 
-
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -11,50 +6,38 @@ import java.util.List;
 
 public class Frame extends JFrame implements ActionListener {
 
-    /**
-     */
-    private static final long serialVersionUID = 1L;
+    boolean isGameRunning = false;
 
     JButton[] buttons;
-
     JFrame window = new JFrame("Chasing-bombs-tp371");
 
+    
     // panels
     JPanel panel_01 = new JPanel();
     JPanel panel_02 = new JPanel();
     JPanel panel_03 = new JPanel();
-
     JPanel panel_inside_02 = new JPanel();
     JPanel panel_inside_03 = new JPanel();
 
-    // button
-    JButton click = new JButton();
 
-    // buttons second panel
-    JButton gameStart = new JButton("Play A Game");
-    JButton exit = new JButton("Exit");
+    //menu
+    JButton start, exit;
 
-    // buttons third panel for difficulty
-    JButton easy = new JButton("Easy");
-    JButton medium = new JButton("Medium");
-    JButton hard = new JButton("Hard");
+    //difficuly
+    JButton easy, medium , hard;
 
-    // Random Variable
-    /**
-     * Use this variable to change difficulty of the game. Depending on the variable
-     * number chose it will change the number of mines being hidden in the cells
-     * therefore causing a change of difficulty.
-     */
+    //randomiser variables
     int changeableValue = 3;
     Random rand = new Random();
-    
     boolean fieldIsBomb = false;
 
     // buttons that will be bombs
     List<String> bombs = new ArrayList<>();
 
     // This is used for debug
-    List<String> debugger = new ArrayList<>();
+    List<String> identifiedBombs = new ArrayList<>();
+
+    boolean methodHasrun = false;
 
     public Frame() {
 
@@ -67,79 +50,46 @@ public class Frame extends JFrame implements ActionListener {
 
         panel_01.setLayout(new GridLayout(2, 5));
 
+        
         for (int i = 0; i < buttons.length; i++) {
             buttons[i] = new JButton(btn[i]);
             buttons[i].setBackground(Color.BLACK);
             buttons[i].setForeground(Color.BLACK);
-            buttons[i].addActionListener(this);
             panel_01.add(buttons[i]);
+            
+            
         }
+        
+        /**
+         * TODO:
+         * -[x] create menu for start and exit
+         * -[x] create menu for difficulty 
+         */
 
-        // randomiser
-        for (int j = 0; j <= changeableValue; j++) {
-            int random = (int) (Math.random() * 10);
-            bombs.add(buttons[random].getText());
-            buttons[random].setText("bomb");
-        }
-        System.out.println(bombs);
+        
+        start = new JButton("Start");
+        exit = new JButton("Exit");
 
-        for (int x = 0; x < buttons.length; x++) {
-
-            debugger.add(buttons[x].getText());
-        }
-        System.out.println(debugger);
-
-        panel_02.add(panel_inside_02);
-        panel_inside_02.setLayout(new GridLayout(2, 1));
-        panel_inside_02.add(gameStart);
-        gameStart.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("This is game start button");
-            }
-        });
+        panel_inside_02.setLayout(new GridLayout(2,1));
+        panel_inside_02.add(start);
+        
         panel_inside_02.add(exit);
-        exit.addActionListener(new ActionListener() {
+        panel_02.add(panel_inside_02);
+       
+        easy = new JButton("Easy");
+        medium = new JButton("Medium");
+        hard= new JButton("Hard");
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("This is exit button");
-            }
-        });
-
-        panel_03.add(panel_inside_03);
-        panel_inside_03.setLayout(new GridLayout(3, 1));
-
+        panel_inside_03.setLayout(new GridLayout(3,1));
         panel_inside_03.add(easy);
-        easy.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeableValue = 5;
-                System.out.println("Value of random bombs is " + changeableValue);
-            }
-        });
-
+        easy.addActionListener(this);
         panel_inside_03.add(medium);
-        medium.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeableValue = 7;
-                System.out.println("Value of random bombs is " + changeableValue);
-            }
-        });
-
+        medium.addActionListener(this);
         panel_inside_03.add(hard);
-        hard.addActionListener(new ActionListener() {
+        hard.addActionListener(this);
+        panel_03.add(panel_inside_03);
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                changeableValue = 9;
-                System.out.println("Value of random bombs is " + changeableValue);
-            }
-        });
+        
 
         window.add(panel_01, BorderLayout.WEST);
         window.add(panel_02, BorderLayout.CENTER);
@@ -149,24 +99,126 @@ public class Frame extends JFrame implements ActionListener {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
 
+        
+        randomiser();
+        
+        
+       
     }
 
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        JButton pressedButton = (JButton)event.getSource();
-        if(pressedButton.getText().equals("bomb")){
-            System.out.println("is a bomb");
+    
+
+    public void randomiser(){
+        //FIXME: Get the actual number of random items that the difficulty is set on
+        
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].addActionListener(new ActionListener(){
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    JButton pressedButton = (JButton)event.getSource();
+                    if(pressedButton.getText().equals("bomb")){
+                        pressedButton.setBackground(Color.RED);
+                    }
+                    else{
+                        pressedButton.setBackground(Color.GREEN);
+                    }
+                }
+            });
         }
-        else{
-            System.out.println("not a bomb");
+
+        
+
+
+        for (int j = 0; j < changeableValue; j++) {
+            int random = (int) (Math.random() * 10);
+            bombs.add(buttons[random].getText());
+            buttons[random].setText("bomb");
         }
+        System.out.println(bombs);
          
+        
+        /* this is for later debbuging 
+        for (int x = 0; x < buttons.length; x++) {
+
+            identifiedBombs.add(buttons[x].getText());
+        }
+        System.out.println(identifiedBombs);*/
     }
+
+    
 
     public static void main(String[] args) {
         Frame game = new Frame();
     }
 
+
+    public void startGame(){
+        start.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent eventStart){
+                
+            }
+
+        });
+    }
+    
+    public void difficultySet(){
+
+        easy.addActionListener(new ActionListener(){
+            
+            @Override
+            public void actionPerformed(ActionEvent easyEvent) {
+                if(easyEvent.getSource() == easy){
+                    if(!medium.isEnabled() && !hard.isEnabled()){
+                        changeableValue = 5;
+                    }
+                }
+               
+            }
+        });
+        medium.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent mediumEvent) {
+                if(mediumEvent.getSource() == medium){
+                    if(!easy.isEnabled() && !hard.isEnabled()){
+                        changeableValue = 7;
+                    }
+                }
+            }
+        });
+        hard.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent hardEvent) {
+                if(hardEvent.getSource() == easy){
+                    if(!easy.isEnabled() && !medium.isEnabled()){
+                        changeableValue = 9;
+                    }
+                }
+            }
+        });
+
+        methodHasrun = true;
+    }
+
+    public void gameOver(){
+
+    }
+
+    public void exitGame(){
+        exit.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent eventExit){
+
+            }
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+        
+    }
     
 }
+
 
